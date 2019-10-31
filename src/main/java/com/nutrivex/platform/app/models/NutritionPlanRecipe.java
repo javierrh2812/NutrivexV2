99bb61f8cbafe2ab2291ebe914ad38e07228dba6
Frankox18 <u201718179@upc.edu.pe>
2019-10-30 21:34:12 -0500

    Se puso la entidad correspondiente

---
+++ src/main/java/com/nutrivex/platform/app/models/NutritionPlanRecipe.java
@@ -0,0 +1,57 @@
+package com.nutrivex.platform.app.models;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "nutrition_plan_recipes")
+public class NutritionPlanRecipe implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id; 
+	
+	@Column(name = "food_date")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date foodDate;
+	
+	@Column(name = "food_number")
+	private int foodNumber;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Date getFoodDate() {
+		return foodDate;
+	}
+
+	public void setFoodDate(Date foodDate) {
+		this.foodDate = foodDate;
+	}
+
+	public int getFoodNumber() {
+		return foodNumber;
+	}
+
+	public void setFoodNumber(int foodNumber) {
+		this.foodNumber = foodNumber;
+	}
+	
+	
+}
