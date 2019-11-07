546ddda2bce7fd508a8b9e14194eaa3af7da77be
Frankox18 <u201718179@upc.edu.pe>
2019-11-06 20:54:44 -0500

    Restore

--- src/main/java/com/nutrivex/platform/app/models/Person.java
+++ src/main/java/com/nutrivex/platform/app/models/Person.java
@@ -37,13 +37,12 @@ public class Person implements Serializable {
 	@Column(name = "sex")
 	private String sex;
 	
-<<<<<<< HEAD
+
 	@Column(name = "nutrionist_spreciality")
 	private String nutrionistSpeciality;
 	
-	@Column(name = "tuition_number")
-	private Long tuitionNumber;
-=======
+	
+
 	@Column(name="nutritionist_specialty")
 	private String nutritionistSpecialty;
 	
@@ -52,7 +51,7 @@ public class Person implements Serializable {
 	
 	@OneToOne(mappedBy = "person", fetch = FetchType.LAZY)
 	private User user;	
->>>>>>> Javier
+
 	
 	//PUBLIC METHODS// 
 	
@@ -66,15 +65,7 @@ public class Person implements Serializable {
 	}
 
 
-	public Long getTuitionNumber() {
-		return tuitionNumber;
-	}
-
-
-	public void setTuitionNumber(Long tuitionNumber) {
-		this.tuitionNumber = tuitionNumber;
-	}
-
+	
 
 	public Person() {
 		super();
