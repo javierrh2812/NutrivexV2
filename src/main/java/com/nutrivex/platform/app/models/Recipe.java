dbbac560873f68e18aa1cb8935ab58594c43311d
Frankox18 <u201718179@upc.edu.pe>
2019-11-08 12:24:54 -0500

    Agregando clase de plan de nutricion

--- src/main/java/com/nutrivex/platform/app/models/Recipe.java
+++ src/main/java/com/nutrivex/platform/app/models/Recipe.java
@@ -32,9 +32,41 @@ public class Recipe implements Serializable{
 	@JoinColumn(name = "recipe_id")
 	private List<FoodRecipe> items;
 	
-	/**
-	 * 
-	 */
+	@OneToMany(mappedBy = "recipes")
+	private List<NutritionPlanRecipe> nutritionPlanRecipe;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<FoodRecipe> getItems() {
+		return items;
+	}
+
+	public void setItems(List<FoodRecipe> items) {
+		this.items = items;
+	}
+
 	private static final long serialVersionUID = 1L;
 	
 	
