4e874bc8c4990b1fa78497e4697832f0cd159c77
Javier <u201719648@upc.edu.pe>
2019-11-09 18:20:02 -0500

    merge con franco

--- src/main/java/com/nutrivex/platform/app/controller/AppController.java
+++ src/main/java/com/nutrivex/platform/app/controller/AppController.java
@@ -4,12 +4,18 @@ package com.nutrivex.platform.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppController {
 		
 	@GetMapping(value={"/", "", "/login"})
 	public String index() {
+		return "login";
+	}
+	
+	@PostMapping(value={"/login"})
+	public String logout() {
 		return "login";
 	}
 }
