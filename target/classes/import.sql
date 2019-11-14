2b6982fb3dce7fbe60ddfed5f800645bd93cdf85
Cristina133 <u201625888@upc.edu.pe>
2019-11-13 20:35:36 -0500

    Merge Franco

--- target/classes/import.sql
+++ target/classes/import.sql
@@ -3,8 +3,6 @@ INSERT INTO roles (name) VALUES ("NUTRITIONIST");
 INSERT INTO roles (name) VALUES ("PATIENT");
 INSERT INTO roles (name) VALUES ("ADMIN");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 INSERT INTO people (first_name, last_name, nutritionist_specialty, tuition_number) VALUES ('Ana', 'Herrera', 'Deportiva', '85210');
 INSERT INTO people (first_name, last_name, nutritionist_specialty, tuition_number) VALUES ('Lucio', 'Ramos', 'Perinatal', '52822');
 INSERT INTO people (first_name, last_name, nutritionist_specialty, tuition_number) VALUES ('Roberto', 'Perez', 'Deportiva', '72528');
@@ -23,9 +21,7 @@ INSERT INTO users (person_id, role_id) VALUES (5,2);
 INSERT INTO users (person_id, role_id) VALUES (6,2);
 INSERT INTO users (person_id, role_id) VALUES (7,2);
 INSERT INTO users (person_id, role_id) VALUES (8,2);
-=======
-=======
->>>>>>> 820a6197b3456f3351987177bb71fab138049fb4
+
 INSERT INTO people (first_name, last_name, nutritionist_specialty, tuition_number) VALUES ('Pedro', 'Garcia', 'Nutricion Deportiva', '1');
 INSERT INTO people (first_name, last_name, nutritionist_specialty, tuition_number) VALUES ('Juan', 'Perez', 'Nutricion Pediatrica', '2');
 INSERT INTO people (first_name, last_name, nutritionist_specialty, tuition_number) VALUES ('Linus', 'Torvalds', 'Nutricion y dietetica', '3');
@@ -44,11 +40,7 @@ INSERT INTO users (id, email, password, person_id, role_id) VALUES (5, "fgregori
 INSERT INTO users (id, email, password, person_id, role_id) VALUES (6, "jramos@correo.com","1234",6,2);
 INSERT INTO users (id, email, password, person_id, role_id) VALUES (7, "ahurtado@correo.com","1234",7,2);
 INSERT INTO users (id, email, password, person_id, role_id) VALUES (8, "jhuarachi@correo.com","1234",8,2);
-<<<<<<< HEAD
->>>>>>> 698bd9a54f76aa94bb17159f4c41efa80e2fe2ab
-=======
 
->>>>>>> 820a6197b3456f3351987177bb71fab138049fb4
 
 INSERT INTO foods (name, type, portion, calories, carbs, protein) VALUES ('Tilapia','Pescado', 'tres cuartos', 11.0, 20.5, 34.7)
 INSERT INTO foods (name, type, portion, calories, carbs, protein) VALUES ('Arroz','Cereal', 'mitad', 11.0, 50.3, 3.9)
