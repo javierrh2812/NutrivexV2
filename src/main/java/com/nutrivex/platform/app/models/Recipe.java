c4f28a7c62784149c45dc3327dd5542f34964eee
Cristina133 <54128487+Cristina133@users.noreply.github.com>
2019-11-19 19:19:34 -0500

    Update Recipe.java

--- src/main/java/com/nutrivex/platform/app/models/Recipe.java
+++ src/main/java/com/nutrivex/platform/app/models/Recipe.java
@@ -69,7 +69,5 @@ public class Recipe implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	@ManyToMany(mappedBy="recipes", fetch = FetchType.LAZY)
-	private List<Template> templates;
 
 }
